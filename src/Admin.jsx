9e9f9753ed1241052d0a8954049f8dbd771ac488
Nicksl2403 <nicolasfaustino2012@gmail.com>
2026-08-18 16:19:05 -0300

    viacaria

--- src/Admin.jsx
+++ src/Admin.jsx
@@ -5,7 +5,7 @@ import "./indexADM.css";
 // URL DO BACKEND
 // ==========================================
 
-const URL_BACKEND = "";
+const URL_BACKEND = "postgresql://neondb_owner:npg_rqgbojUsY3X5@ep-sparkling-dawn-acqn4983-pooler.sa-east-1.aws.neon.tech/neondb?channel_binding=require&sslmode=require";
 
 
 function Admin() {
