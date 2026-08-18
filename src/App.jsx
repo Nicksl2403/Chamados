89cad757e1df696d527c53891e3adec4344803da
Nicksl2403 <nicolasfaustino2012@gmail.com>
2026-08-18 16:01:53 -0300

    dale

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ import redeAviso from "./assets/wifiAviso.svg";
 // URL DO BACKEND
 // ==========================================
 
-const URL_BACKEND = "COLE_A_URL_DO_SEU_BACKEND_AQUI";
+const URL_BACKEND = "";
 
 
 function App() {
